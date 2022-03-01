880e502fa91009c51f340b60d11f8e74f1486c09
Somi Jeon <iam.somj@gmail.com>
2022-02-28 18:44:29 -0800

    Save html object using DOM

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`)); // express.static built-in middleware -> To serve static files such as images, CSS files, and JavaScript files
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginButton = document.querySelector("button"); // 태그 이름
+
+loginButton.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
